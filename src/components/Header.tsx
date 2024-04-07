7e14701cf5ee16978a07eaba469ab86737776d17
Kürşat Doğan Çelik <kursatdogancelik@gmail.com>
2024-04-08 02:45:42 +0300

    yeaa

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,7 +1,6 @@
 'use client';
 import { useState } from 'react';
-import { Dialog, Popover } from '@headlessui/react';
-import { Bars3Icon, XMarkIcon } from '@heroicons/react/24/outline';
+import { BsList, BsXLg } from 'react-icons/bs';
 import Image from 'next/image';
 import Link from 'next/link';
 import logo from '/public/images/logo.png';
@@ -12,96 +11,70 @@ export default function Header() {
 
   return (
     <header className="bg-white">
-      <nav className="mx-auto flex max-w-7xl items-center justify-between p-6 md:px-8" aria-label="Global">
-        <div className="flex md:flex-1">
-          <Link href="/" className="-m-1.5 p-1.5">
-            <span className="sr-only">Gno Calculator</span>
-            <Image className="h-8 w-auto" src={logo} alt="Logo" />
+      <nav className="mx-auto flex items-center justify-between px-3 py-3 md:px-8">
+        <Image className="size-12" src={logo} alt="Gno Calculator" />
+
+        <button onClick={() => setMobileMenuOpen(true)} className="flex cursor-pointer p-3 md:hidden">
+          <BsList className="size-6 text-gray-700" />
+        </button>
+
+        <div className="hidden text-sm font-semibold leading-6 text-gray-900 md:flex md:gap-6">
+          <Link href="/" className="m-2 p-1">
+            Home
           </Link>
-        </div>
-        <div className="flex md:hidden">
-          <button
-            type="button"
-            className="-m-2.5 inline-flex items-center justify-center rounded-md p-2.5 text-gray-700"
-            onClick={() => setMobileMenuOpen(true)}
-          >
-            <span className="sr-only">Open main menu</span>
-            <Bars3Icon className="h-6 w-6" aria-hidden="true" />
-          </button>
-        </div>
-        <Popover.Group className="hidden md:flex md:gap-x-12">
-          <ThemeSwitch />
-          <Link href="/features" className="text-sm font-semibold leading-6 text-gray-900">
-            Features
+          <Link href="/" className="m-2 p-1">
+            Contact
           </Link>
-          <Link href="#" className="text-sm font-semibold leading-6 text-gray-900">
-            Marketplace
+          <Link href="/" className="m-2 p-1">
+            Help
           </Link>
-          <Link href="#" className="text-sm font-semibold leading-6 text-gray-900">
-            Company
-          </Link>
-        </Popover.Group>
-        <div className="hidden md:flex md:flex-1 md:justify-end">
-          <Link href="#" className="text-sm font-semibold leading-6 text-gray-900">
-            Log in <span aria-hidden="true"></span>
+          <ThemeSwitch className="m-3" />
+          <Link href="/" className="m-2 p-1">
+            Login
           </Link>
         </div>
       </nav>
-      <Dialog as="div" className="md:hidden" open={mobileMenuOpen} onClose={setMobileMenuOpen}>
-        <div className="fixed inset-0 z-10" />
-        <Dialog.Panel className="fixed right-0 top-0 z-10 w-full overflow-y-auto bg-white px-6 py-6 sm:max-w-sm sm:ring-1 sm:ring-gray-900/10">
+
+      {mobileMenuOpen && (
+        <div className="fixed right-0 top-0 z-10 w-80 bg-white px-6 py-6 md:hidden">
           <div className="flex items-center justify-between">
-            <Link href="#" className="-m-1.5 p-1.5 sm:max-md:invisible">
-              <span className="sr-only">Your Company</span>
-              <img
-                className="h-8 w-auto"
-                src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=600"
-                alt=""
-              />
-            </Link>
-            <button
-              type="button"
-              className="-m-2.5 rounded-md p-2.5 text-gray-700"
-              onClick={() => setMobileMenuOpen(false)}
-            >
-              <span className="sr-only">Close menu</span>
-              <XMarkIcon className="h-6 w-6" aria-hidden="true" />
+            <ThemeSwitch />
+            <button onClick={() => setMobileMenuOpen(false)} className="-m-2.5 rounded-md p-2.5 text-gray-700">
+              <BsXLg className="size-6 text-gray-700" />
             </button>
           </div>
-          <div className="mt-6 flow-root">
-            <div className="-my-6 divide-y divide-gray-500/10">
-              <div className="space-y-2 py-6">
-                <Link
-                  href="/features"
-                  className="-mx-3 block rounded-md px-3 py-2 text-base font-semibold leading-7 text-gray-900 hover:bg-gray-50"
-                >
-                  Features
-                </Link>
-                <Link
-                  href="#"
-                  className="-mx-3 block rounded-md px-3 py-2 text-base font-semibold leading-7 text-gray-900 hover:bg-gray-50"
-                >
-                  Marketplace
-                </Link>
-                <Link
-                  href="#"
-                  className="-mx-3 block rounded-md px-3 py-2 text-base font-semibold leading-7 text-gray-900 hover:bg-gray-50"
-                >
-                  Company
-                </Link>
-              </div>
-              <div className="py-6">
-                <Link
-                  href="#"
-                  className="-mx-3 block rounded-md px-3 py-2.5 text-base font-semibold leading-7 text-gray-900 hover:bg-gray-50"
-                >
-                  Log in
-                </Link>
-              </div>
+
+          <div className=" divide-y ">
+            <div className="space-y-2 pt-6">
+              <Link
+                href="/"
+                className="-mx-3 block rounded-md px-3 py-2 text-base font-semibold text-gray-900 hover:bg-gray-50"
+              >
+                Home
+              </Link>
+              <Link
+                href="/"
+                className="-mx-3 block rounded-md px-3 py-2 text-base font-semibold text-gray-900 hover:bg-gray-50"
+              >
+                Contact
+              </Link>
+              <Link
+                href="/"
+                className="-mx-3 block rounded-md px-3 py-2 text-base font-semibold text-gray-900 hover:bg-gray-50"
+              >
+                Help
+              </Link>
             </div>
+
+            <Link
+              href="/"
+              className="-mx-3 mt-2 block rounded-md px-3 py-2.5 text-base font-semibold text-gray-900 hover:bg-gray-50"
+            >
+              Login
+            </Link>
           </div>
-        </Dialog.Panel>
-      </Dialog>
+        </div>
+      )}
     </header>
   );
 }
--- src/components/ThemeSwitch.tsx
+++ src/components/ThemeSwitch.tsx
@@ -1,13 +1,11 @@
 'use client';
-
 import { useTheme } from 'next-themes';
 import Image from 'next/image';
 import { useState, useEffect } from 'react';
 import logo from '/public/images/logo.png';
 import { BsSun, BsMoon } from 'react-icons/bs';
 
-
-export default function ThemeSwitch() {
+export default function ThemeSwitch(props: { className?: string }) {
   const [mounted, setMounted] = useState(false);
   const { setTheme, resolvedTheme } = useTheme();
 
@@ -15,10 +13,20 @@ export default function ThemeSwitch() {
   if (!mounted) return <Image src={logo} width={24} height={24} alt="ehehe" />;
 
   if (resolvedTheme === 'dark') {
-    return <BsSun className="size-6 cursor-pointer bg-black" onClick={() => setTheme('light')} />;
+    return (
+      <BsSun
+        className={`${props.className} size-6 cursor-pointer bg-white text-black`}
+        onClick={() => setTheme('light')}
+      />
+    );
   }
 
   if (resolvedTheme === 'light') {
-    return <BsMoon className="size-6 cursor-pointer bg-white" onClick={() => setTheme('dark')} />;
+    return (
+      <BsMoon
+        className={`${props.className} size-6 cursor-pointer bg-white text-black`}
+        onClick={() => setTheme('dark')}
+      />
+    );
   }
 }
